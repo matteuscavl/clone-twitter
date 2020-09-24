ca78c070a3d0a9f56763c0a15bf0ba9a865b5b66
matteuscavl <mateuslcavl@gmail.com>
2020-09-24 15:40:24 -0300

    Alteracoes na main e no codigo JS

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+const btnTweetar = $('.btn-enviar')
+
+btnTweetar.click(function() {
+    var inputtweet = $('.input-tweetar')
+    // console.log(inputtweet.val());
+})
+
+const renderContainer = () => {
+
+}
